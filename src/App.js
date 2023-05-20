b5b047624ff9ad323e0424d8357ffd7d756330d9
Chege Ian <chegeian65@gmail.com>
2023-05-20 18:10:46 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Navbar from "./components/Navbar/Navbar"
+import Home from "./components/Home /Home"
+import About from "./components/About/About"
+import Skills from "./components/Skills/Skills"
+import Contact from "./components/Contact/Contact"
+import Services from "./components/Services/Services"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Navbar />
+
+        <Switch>
+          <Route path='/' components={Home} exact>
+            <Home />
+          </Route>
+          <Route path='/about' components={About} exact>
+            <About />
+          </Route>
+          <Route path='/skills' components={Skills} exact>
+            <Skills />
+          </Route>
+          <Route path='/contact' components={Contact} exact>
+            <Contact />
+          </Route>
+          <Route path='/services' components={Services} exact>
+            <Services />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Home /Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+  return (
+    <>
+    <div className="homepage-lander">
+    <div className="homepage-tile">
+        <p className="sub-heading">THE WAY IN WHICH WE TRAIN IS OUTDATED</p>
+        <h1 className="heading">We are a group of companies which unlock true human potential.</h1>
+        <p className="sub-heading">Irrespective of origin, nationality, gender, and belief. </p>
+    </div>
+</div>
+  </>
+  )
+}
+export default Home;
